7ba4fb20058f30d2b245d2e4d121a8fcf25f55a2
Robert Shaw <robertshaw87@gmail.com>
2018-05-22 15:01:12 -0700

    add initial play logic

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ var inquirer = require("inquirer");
 
 var targetWord;
 
+selectWord();
+
 function selectWord() {
-    targetWord = new Word("Hello World")
+    targetWord = new Word("Hello World");
+    playHangman();
 }
 
 function playHangman() {
-
+    console.log(targetWord.displayWord());
+    inquirer.prompt({
+        type: "input",
+        message: "Please select a letter from a-z.",
+        name: "userLetter",
+        validate: function(letter){
+            var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+            return (alphabet.indexOf(letter.toLowerCase()) !== -1);
+        }
+    }).then(function(answer){
+        targetWord.guess(answer.userLetter);
+        if (targetWord.completed()){
+            console.log(targetWord.displayWord());
+            console.log("Congratulations! You've succesfully guessed the word!")
+        } else {
+            playHangman();
+        }
+    })
 }
